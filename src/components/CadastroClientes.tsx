94e65d507e6538d6063c3a60c9defaf2e6699515
eduardo01322 <eduardo.errea32@gmail.com>
2023-10-26 14:04:42 -0300

    .

---
+++ src/components/CadastroClientes.tsx
@@ -0,0 +1,131 @@
+import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import axios from 'axios';
+import Header from './Header';
+import Footer from './Footer';
+
+const Cadastro = () => {
+
+    const [nome, setNome] = useState<string>("");
+    const [celular, setCelular] = useState<Number>();
+    const [email, setEmail] = useState<string>("");
+    const [cpf, setCpf] = useState<string>("");
+    const [dataDeNascimento, setDataDeNascimento] = useState<Date>();
+    const [cidade, setCidade] = useState<string>("");
+    const [estado, setEstado] = useState<string>("");
+    const [pais, setPais] = useState<string>("");
+    const [rua, setRua] = useState<string>("");
+    const [numero, setNumero] = useState<number>();
+    const [bairro, setBairro] = useState<string>("");
+    const [cep, setCep] = useState<string>("");
+    const [complemento, setComplemento] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const[erro, setErro]=useState<string>("")
+
+
+    const Cep = (e: FormEvent) => {
+        e.preventDefault();
+        fetch('https://viacep.com.br/ws/'+cep+'/json/',
+        {
+            method:'GET'
+        }).then(Response=> Response.json())
+        .then(
+            data=>{
+                setCidade(data.localidade);
+                setCep(data.cep);
+                setEstado(data.estado);
+                setPais(data.pais);
+                setRua(data.rua);
+                setNumero(data.numero);
+                setBairro(data.bairro);
+                setErro("")
+            }
+        ).catch(error => {
+            setErro("Pesquisa Inválida");
+        });
+
+    }
+
+    const cadastrarUsuario = (e: FormEvent) => {
+        e.preventDefault();
+        const dados = {
+            nome: nome,
+            email: email,
+            celular:celular,
+            cpf: cpf,
+            dataDeNascimento: dataDeNascimento,
+            cidade: cidade,
+            estado: estado,
+            pais: pais,
+            rua: rua,
+            numero: numero,
+            bairro: bairro,
+            cep: cep,
+            complemento: complemento,
+            password: password
+        }
+
+        console.log(dados)
+        axios.post('http://10.137.9.131:8000/api/store', dados, {
+            headers:
+                { "Accept": "application/json", "Content-Type": "application/json" }
+        }).then(function (response) {
+            window.location.href = "/listagem"
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    const handleState = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "nome") {
+            setNome(e.target.value);
+        }
+        if (e.target.name === "email") {
+            setEmail(e.target.value);
+        }
+        if (e.target.name === "cpf") {
+            setCpf(e.target.value);
+        }
+        if (e.target.name === "password") {
+            setPassword(e.target.value);
+        }
+    }
+
+    return (
+        <div>
+            <Header />
+            <main className={styles.main}>
+                <div className='container'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h5 className='card-title'>Cadastrar Clientes</h5>
+                            <form onSubmit={cadastrarUsuario} className='row g-3'>
+                                <div className='col-6'>
+                                    <label htmlFor="nome" className='form-label'>Nome</label>
+                                    <input type="text" name='nome' className='form-control' required onChange={handleState} />{nome}
+                                </div>
+                                <div className='col-6'>
+                                    <label htmlFor="email" className='form-label'>E-mail</label>
+                                    <input type="text" name='email' className='form-control' required onChange={handleState} />
+                                </div>
+                                <div className='col-6'>
+                                    <label htmlFor="cpf" className='form-label'>CPF</label>
+                                    <input type="text" name='cpf' className='form-control' required onChange={handleState} />
+                                </div>
+                                <div className='col-6'>
+                                    <label htmlFor="password" className='form-label'>Senha</label>
+                                    <input type="text" name='password' className='form-control' required onChange={handleState} />
+                                </div>
+                                <div className='col-12'>
+                                    <button type='submit' className='btn btn-dark btn-sm'>Cadastrar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default Cadastro;
---
+++ src/interfaces/CadastroClienteInterface.ts
@@ -0,0 +1,17 @@
+export interface CadastroInterface{
+    id:number;
+    nome:string;
+    celular:number;
+    email:string;
+    cpf:string;
+    dataDeNascimento:Date;
+    cidade:string;
+    estado:string;
+    pais:string;
+    rua:string;
+    numero:number;
+    bairro:string;
+    cep:string;
+    complemento:string;
+    passaword:string;
+}
